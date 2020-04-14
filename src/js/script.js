fb797360887122e2b9157411952154b47930b72c
Jovana <jovana.jovanovska@poviolabs.com>
2020-04-14 20:22:25 +0200

    Update content and dependencies

--- src/js/script.js
+++ src/js/script.js
@@ -5,3 +5,10 @@ var square = document.getElementById('square');
 square.addEventListener('webkitAnimationEnd',function( event ) {
   square.style.display = 'none';
 }, false);
+
+
+document.addEventListener("scroll", function() {
+  console.log("im in");
+  // var navbar = querySelector(".navbar");
+  // navbar.classList.add("bg-white");
+});
